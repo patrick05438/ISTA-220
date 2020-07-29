70094f211035d43b82f4e2bf4ad24b45455e71f9
Patrick Juarez <patrick.juarez1@outlook.com>
2020-07-28 20:03:43 -0400

    Quiz

---
+++ Quiz04-Juarez/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Quiz04_Juarez
+{
+    class Firearm
+    {
+        public void fire(string name, string noise)
+        {
+            Console.WriteLine($"I am a {name} and I go {noise}.");
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+                Console.WriteLine("This is Quiz 04");
+                Firearm Shotgun = new Firearm();
+                Shotgun.fire("Shotgun", "Boom");
+                Firearm Rifle = new Firearm();
+                Rifle.fire("Rifle", "Bang");
+                Firearm Pistol = new Firearm();
+                Pistol.fire("Pistol", "Pop");
+                Console.WriteLine("                  ");
+                Console.WriteLine("This is Quiz 04");
+                Firearm Howitzer = new Firearm();
+                Howitzer.fire("Howitzer", "Boom");
+                Firearm BigGuns = new Firearm();
+                BigGuns.fire("Big Guns", "Boom Boom");
+                Firearm LilGuns = new Firearm();
+                LilGuns.fire("Lil Guns", "To the 82nd");
+
+            }
+        }
+    }
+}
---
+++ Quiz3-Juarez/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+
+namespace Quiz3_Juarez
+{
+    class Program
+    {
+        private static object result;
+
+        static void Main(string[] args)
+        {
+            daReprical();
+        }
+
+        private static void daReprical()
+        {
+            int num;
+            Console.Write("To calculate the reciprocal of an integer, enter a positive integer: ");
+            string usernum = Console.ReadLine();
+            try
+            {
+                num = int.Parse(usernum);
+                if (num < 0)
+                    throw new Exception("Your number must be a positive number");
+                double recip = (double)1.0 / num;
+                if (double.IsInfinity(recip))
+                    throw new DivideByZeroException("Attempted to divide by zero");
+                Console.WriteLine($"The reciprocal is {recip}");
+            }
+            catch (FormatException fex)
+            {
+                Console.WriteLine(fex.Message);
+                daReprical();
+            }
+
+            
+            catch (DivideByZeroException dEx)
+            {
+                Console.WriteLine(dEx.Message);
+                daReprical();
+            }
+            
+            catch (Exception Ex)
+
+            {
+                Console.WriteLine(Ex.Message);
+                daReprical();
+            }
+                   
+        }
+    }
+}
+
